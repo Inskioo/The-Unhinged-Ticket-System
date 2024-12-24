46b8a7e2d10a1b4210ec2608fae1bea74e934432
Rebecca Inski <Bex@inski.io>
2024-12-24 15:18:36 +0000

    Generate Ticket
    Added command to generate our ticket.

---
+++ unhinged/app/Console/Commands/GenerateTicket.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace App\Console\Commands;
+
+use App\Models\Ticket;
+use Illuminate\Console\Command;
+
+class GenerateTicket extends Command{
+    protected $signature = 'ticket:generate';
+    protected $description = 'A customer has a problem not quite of this world. Log it with a unhinged ticket';
+
+    public function handle(){
+        $ticket = ticketFactory()->create();
+        $this->info('I\'m sorry, we have logged ticket : ' . $ticket->id);
+    }
+}
